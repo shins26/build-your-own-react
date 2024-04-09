d1ba2dbaa75ad8bb07bd5ab7c6395c5ad1b0c605
Shinya Sakai <shinya.sakai.26@gmail.com>
2024-04-09 19:24:20 +0900

    TS2307 Cannot find module

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const element = <h1 title="foo">Hello</h1>;
+const container = document.getElementById("root");
+if (!container) throw new Error("No root Element");
+
+const root = createRoot(container);
+root.render(element);
