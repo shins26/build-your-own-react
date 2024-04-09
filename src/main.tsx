8696bd0cd0f47e2f80b6bc51a81bde5dc009d60d
Shinya Sakai <shinya.sakai.26@gmail.com>
2024-04-10 00:01:50 +0900

    Add Step 0: Review

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,19 @@
-import { createRoot } from "react-dom/client";
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "Hello",
+  },
+};
 
-const element = <h1 title="foo">Hello</h1>;
 const container = document.getElementById("root");
 if (!container) throw new Error("No root Element");
 
-const root = createRoot(container);
-root.render(element);
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+
+node.appendChild(text);
+container.appendChild(node);
