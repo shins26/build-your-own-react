fa91cd7e80814e6123e411d9b20cc86f2dd9360e
Shinya Sakai <shinya.sakai.26@gmail.com>
2024-04-10 19:57:33 +0900

    Add Step 1: The createElement Function

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,54 @@
-const element = {
-  type: "h1",
-  props: {
-    title: "foo",
-    children: "Hello",
-  },
+type DidactText = string | number;
+type DidactChild = DidactElement | DidactText;
+
+type DidactElement = {
+  type: string;
+  props: DidactProps;
 };
+
+type DidactProps = {
+  nodeValue?: DidactText;
+  children?: DidactElement[];
+  [key: string]: any;
+} | null;
+
+function createElement(
+  type: string,
+  props: DidactProps = {},
+  ...children: DidactChild[]
+) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text: DidactText) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const Didact = {
+  createElement,
+};
+
+const element = Didact.createElement(
+  "div",
+  { id: "foo" },
+  Didact.createElement("a", null, "bar"),
+  Didact.createElement("b")
+);
+console.log("element = ", element);
 
 const container = document.getElementById("root");
 if (!container) throw new Error("No root Element");
-
-const node = document.createElement(element.type);
-node["title"] = element.props.title;
-
-const text = document.createTextNode("");
-text["nodeValue"] = element.props.children;
-
-node.appendChild(text);
-container.appendChild(node);
