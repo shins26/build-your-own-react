99ab2e40d158df30112802379290357fdc6969d6
Shinya Sakai <shinya.sakai.26@gmail.com>
2024-04-11 21:00:00 +0900

    Add Step 2: The render Function

--- src/main.tsx
+++ src/main.tsx
@@ -14,9 +14,9 @@ type DidactProps = {
 
 function createElement(
   type: string,
-  props: DidactProps = {},
+  props: DidactProps = { children: [] },
   ...children: DidactChild[]
-) {
+): DidactElement {
   return {
     type,
     props: {
@@ -28,7 +28,7 @@ function createElement(
   };
 }
 
-function createTextElement(text: DidactText) {
+function createTextElement(text: DidactText): DidactElement {
   return {
     type: "TEXT_ELEMENT",
     props: {
@@ -38,8 +38,37 @@ function createTextElement(text: DidactText) {
   };
 }
 
+function render(element: DidactElement, container: HTMLElement) {
+  if (element.props === null) return;
+
+  if (element.type === "TEXT_ELEMENT") {
+    const dom = document.createTextNode(
+      typeof element.props.nodeValue === "string" ? element.props.nodeValue : ""
+    );
+    container.appendChild(dom);
+    return;
+  }
+
+  const dom = document.createElement(element.type);
+  const isProperty = (key: string) => key !== "children";
+
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      if (element.props === null) return;
+      dom.setAttribute(name, element.props[name]);
+    });
+
+  if (Array.isArray(element.props.children)) {
+    element.props.children.forEach((child) => render(child, dom));
+  }
+
+  container.appendChild(dom);
+}
+
 const Didact = {
   createElement,
+  render,
 };
 
 const element = Didact.createElement(
@@ -48,7 +77,8 @@ const element = Didact.createElement(
   Didact.createElement("a", null, "bar"),
   Didact.createElement("b")
 );
-console.log("element = ", element);
 
 const container = document.getElementById("root");
 if (!container) throw new Error("No root Element");
+
+Didact.render(element, container);
