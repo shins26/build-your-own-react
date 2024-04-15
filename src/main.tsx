19ab062b40411f2549b79c6c77892cc95ec0f375
Shinya Sakai <shinya.sakai.26@gmail.com>
2024-04-15 15:28:57 +0900

    Add Step 4: Fibers

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,15 @@ type DidactElement = {
 type DidactProps = {
   nodeValue?: DidactText;
   children: DidactElement[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any;
+};
+
+type Fiber = DidactElement & {
+  dom?: HTMLElement | Text;
+  parent?: Fiber;
+  child?: Fiber;
+  sibling?: Fiber;
 };
 
 function createElement(
@@ -38,33 +46,39 @@ function createTextElement(text: DidactText): DidactElement {
   };
 }
 
-function render(element: DidactElement, container: HTMLElement) {
-  if (element.type === "TEXT_ELEMENT") {
-    const dom = document.createTextNode(
-      typeof element.props.nodeValue === "string" ? element.props.nodeValue : ""
+function createDom(fiber: Fiber): HTMLElement | Text {
+  if (fiber.type === "TEXT_ELEMENT") {
+    return document.createTextNode(
+      typeof fiber.props.nodeValue === "string" ? fiber.props.nodeValue : ""
     );
-    container.appendChild(dom);
-    return;
   }
 
-  const dom = document.createElement(element.type);
+  const dom = document.createElement(fiber.type);
   const isProperty = (key: string) =>
     key !== "children" && key !== "__self" && key !== "__source";
 
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom.setAttribute(name, element.props[name]);
+      dom.setAttribute(name, fiber.props[name]);
     });
 
-  element.props.children.forEach((child) => render(child, dom));
-
-  container.appendChild(dom);
+  return dom;
 }
 
-let nextUnitOfWork: unknown = null;
+function render(element: DidactElement, container: HTMLElement): void {
+  nextUnitOfWork = {
+    type: element.type,
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
 
-function workLoop(deadline: IdleDeadline) {
+let nextUnitOfWork: Fiber | undefined = undefined;
+
+function workLoop(deadline: IdleDeadline): void {
   let shouldYield = false;
   while (nextUnitOfWork && !shouldYield) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
@@ -75,8 +89,48 @@ function workLoop(deadline: IdleDeadline) {
 
 requestIdleCallback(workLoop);
 
-function performUnitOfWork(nextUnitOfWork: unknown) {
-  // TODO
+function performUnitOfWork(fiber: Fiber): Fiber | undefined {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent?.dom) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // create new fibers
+  const elements = fiber.props.children;
+  let prevSibling: Fiber | undefined = undefined;
+
+  for (const element of elements) {
+    const newFiber: Fiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: undefined,
+    };
+
+    if (!prevSibling) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+  }
+
+  // return next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber: Fiber | undefined = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
